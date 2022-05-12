9bbc3b91cd2d716ac4261230214eb6b8165aba5e
satyam1209 <103816941+satyam1209@users.noreply.github.com>
2022-05-12 20:24:53 +0530

    Update forms.py

--- CARSIFY2/carsify2/car/forms.py
+++ CARSIFY2/carsify2/car/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,Userna
 from django.contrib.auth.models import User
 from django.utils.translation import gettext,gettext_lazy as _
 
-
+# form for signup that will be rendered in html
 class SignUpForm(UserCreationForm):
 	password1= forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
 	password2= forms.CharField(label='Password(again)',widget=forms.PasswordInput(attrs={'class':'form-control'}))
@@ -16,7 +16,7 @@ class SignUpForm(UserCreationForm):
 		'last_name': forms.TextInput(attrs={'class':'form-control','required':True,}),
 		'email': forms.EmailInput(attrs={'class':'form-control','required':True,}),
 		}
-
+# login form for authentication
 class LoginForm(AuthenticationForm):
 	username=UsernameField(label='UserName',widget=forms.TextInput(attrs={'autofocus':True,'class':'form-control'}))
 	password=forms.CharField(label=_('Pasword'),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
